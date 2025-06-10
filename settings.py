18f5c14e28d32ca719cfebc11535e801acae35e6
pit-rella <serezha.sliva@gmail.com>
2025-06-11 00:28:21 +0300

    - Added .env_example - Added settings from .env

--- settings.py
+++ settings.py
@@ -2,6 +2,10 @@ from envparse import Env
 
 env = Env()
 
-DATABASE_URL = env.str("DATABASE_URL",
-                       default="postgresql+asyncpg://postgres:postgres@0.0.0.0:5432/education_db")
-
+# Settings for JWT
+SECRET_KEY: str = env.str("SECRET_KEY", default="my-secret-key")
+ALGORITHM: str = env.str("ALGORITHM", default="HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES: int = env.int("ACCESS_TOKEN_EXPIRE_MINUTES",
+                                           default=30)
+DATABASE_URL: str = env.str("DATABASE_URL",
+                            default="postgresql+asyncpg://postgres:postgres@0.0.0.0:5432/education_db")
