6b562489e60c66e3c927f41045fb080b70af4211
PitRella <serezha.sliva@gmail.com>
2025-06-15 23:26:16 +0300

    - FEAT(main) - Added versioning for api. Added prefix "api" for every endpoint

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from src.auth.router import auth_router
 
 app = FastAPI(title="EducationPlatform")
 
-main_api_router = APIRouter()
+main_api_router = APIRouter(prefix="/api/v1")
 main_api_router.include_router(user_router, prefix="/user", tags=["user"])
 main_api_router.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(main_api_router)
