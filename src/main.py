6f059364236b1c276c161d8086707ffd30aeca99
PitRella <serezha.sliva@gmail.com>
2025-06-25 23:03:38 +0300

    - FEAT(main) - Installed prometheus - FEAT(docker-compose) - Added service for prometheus, grafana

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import sentry_sdk
 from fastapi import FastAPI, APIRouter
+from starlette_exporter import handle_metrics
+from starlette_exporter import PrometheusMiddleware
 
 from src.settings import SENTRY_URL
 from src.users.router import user_router
@@ -10,7 +12,8 @@ sentry_sdk.init(
     send_default_pii=True,
 )
 app = FastAPI(title="EducationPlatform")
-
+app.add_middleware(PrometheusMiddleware)
+app.add_route("/metrics", handle_metrics)
 main_api_router = APIRouter(prefix="/api/v1")
 main_api_router.include_router(user_router, prefix="/user", tags=["user"])
 main_api_router.include_router(auth_router, prefix="/auth", tags=["auth"])
