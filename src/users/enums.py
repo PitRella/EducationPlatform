84aebcf2c01caa634d3857b5d3f87564fe3c2b2c
PitRella <serezha.sliva@gmail.com>
2025-07-12 16:28:03 +0300

    - FEAT(users) - Added AUTHOR role to UserRoles enum for course author permissions

--- src/users/enums.py
+++ src/users/enums.py
@@ -7,9 +7,11 @@ class UserRoles(StrEnum):
     Defines a hierarchy of user roles with different permission levels:
     - SUPERADMIN: Highest level with full system access
     - ADMIN: Administrative access with elevated permissions
+    - AUTHOR: Course author access with limited permissions
     - USER: Basic user role with standard permissions
     """
 
     SUPERADMIN = 'superadmin'
     ADMIN = 'admin'
+    AUTHOR = 'author'
     USER = 'user'
