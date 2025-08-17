400b36076b38eb4a07a31f9c2a4c199af24ebd0c
PitRella <serezha.sliva@gmail.com>
2025-08-17 15:45:24 +0300

    REF(users/permissions) - Retrieve user from kwarg parameters

--- src/users/permissions/user.py
+++ src/users/permissions/user.py
@@ -1,8 +1,9 @@
 from abc import ABC
+from typing import Unpack
 
 from fastapi.requests import Request
 
-from src.base.permission import BasePermission
+from src.base.permission import BasePermission, PermissionKwargs
 from src.users import User
 from src.users.exceptions import (
     UserNotAuthorizedException,
@@ -13,12 +14,12 @@ from src.users.exceptions import (
 # Enforces a contract for permission validation logic.
 class BaseUserPermission(BasePermission, ABC):
     def __init__(
-        self,
-        user: User | None,
-        request: Request,
+            self,
+            request: Request,
+            **kwargs: Unpack[PermissionKwargs],
     ):
-        super().__init__(request)
-        self.user: User | None = user
+        super().__init__(request, **kwargs)
+        self.user: User | None = kwargs.get('user')
 
     def _is_user_authorized(self) -> User:
         if not self.user:
@@ -28,39 +29,38 @@ class BaseUserPermission(BasePermission, ABC):
 
 class AdminPermission(BaseUserPermission):
     def __init__(
-        self,
-        user: User | None,
-        request: Request,
-        target_user: User,
+            self,
+            request: Request,
+            **kwargs: Unpack[PermissionKwargs],
     ):
-        super().__init__(user=user, request=request)
-        self.target_user = target_user
+        super().__init__(request, **kwargs)
+        self.target_user = kwargs['target_user']
 
     async def validate_permission(
-        self,
+            self,
     ) -> None:
         auth_user: User = self._is_user_authorized()
         if (
-            (  # Superadmin cannot interact with another superadmin
-                auth_user.is_user_superadmin
-                and self.target_user.is_user_superadmin
-            )
-            or (  # Admin cannot interact with another admin
+                (  # Superadmin cannot interact with another superadmin
+                        auth_user.is_user_superadmin
+                        and self.target_user.is_user_superadmin
+                )
+                or (  # Admin cannot interact with another admin
                 auth_user.is_user_admin and self.target_user.is_user_admin
-            )
-            or (  # Admin cannot interact with superadmin
+        )
+                or (  # Admin cannot interact with superadmin
                 auth_user.is_user_admin and self.target_user.is_user_superadmin
-            )
+        )
         ):
             raise UserPermissionException
 
 
 class SuperadminPermission(AdminPermission):
     async def validate_permission(
-        self,
+            self,
     ) -> None:
         auth_user: User = self._is_user_authorized()
         if not auth_user.is_user_superadmin or (
-            self.target_user.is_user_superadmin
+                self.target_user.is_user_superadmin
         ):
             raise UserPermissionException
