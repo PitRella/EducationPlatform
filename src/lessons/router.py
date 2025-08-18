c3793fe0e37b04bcd4e0405bfd8e3ff347423577
PitRella <serezha.sliva@gmail.com>
2025-08-18 23:16:36 +0300

    REF(lessons/router) - Commented endpoint

--- src/lessons/router.py
+++ src/lessons/router.py
@@ -11,14 +11,14 @@ from src.lessons.service import LessonService
 lesson_router = APIRouter()
 
 
-@lesson_router.post('/{course_id}', response_model=LessonResponseSchema)
-async def create_lesson(
-    lesson_schema: CreateLessonRequestSchema,
-    course: Annotated[Course, Depends(get_author_course_by_id)],
-    service: Annotated[LessonService, Depends(get_service(LessonService))],
-) -> LessonResponseSchema:
-    """Endpoint to create a new course."""
-    lesson = await service.create_lesson(
-        course=course, lesson_schema=lesson_schema
-    )
-    return LessonResponseSchema.model_validate(lesson)
+# @lesson_router.post('/{course_id}', response_model=LessonResponseSchema)
+# async def create_lesson(
+#     lesson_schema: CreateLessonRequestSchema,
+#     course: Annotated[Course, Depends(get_author_course_by_id)],
+#     service: Annotated[LessonService, Depends(get_service(LessonService))],
+# ) -> LessonResponseSchema:
+#     """Endpoint to create a new course."""
+#     lesson = await service.create_lesson(
+#         course=course, lesson_schema=lesson_schema
+#     )
+#     return LessonResponseSchema.model_validate(lesson)
