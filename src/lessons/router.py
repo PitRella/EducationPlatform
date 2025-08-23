838f0e958f5f0fc0fccb92dad59397e878e2f1dd
PitRella <serezha.sliva@gmail.com>
2025-08-23 22:10:22 +0300

    FEAT(lessons/router) - Added create lesson endpoint with course author permission validation

--- src/lessons/router.py
+++ src/lessons/router.py
@@ -1,23 +1,34 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
+from fastapi.params import Security
 
 from src.base.dependencies import get_service
+from src.courses.dependencies import CoursePermissionDependency
 from src.courses.models import Course
+from src.courses.permissions import IsAuthorCourse
 from src.lessons.schemas import CreateLessonRequestSchema, LessonResponseSchema
 from src.lessons.service import LessonService
 
 lesson_router = APIRouter()
 
 
-# @lesson_router.post('/{course_id}', response_model=LessonResponseSchema)
-# async def create_lesson(
-#     lesson_schema: CreateLessonRequestSchema,
-#     course: Annotated[Course, Depends(get_author_course_by_id)],
-#     service: Annotated[LessonService, Depends(get_service(LessonService))],
-# ) -> LessonResponseSchema:
-#     """Endpoint to create a new course."""
-#     lesson = await service.create_lesson(
-#         course=course, lesson_schema=lesson_schema
-#     )
-#     return LessonResponseSchema.model_validate(lesson)
+@lesson_router.post('/{course_id}', response_model=LessonResponseSchema)
+async def create_lesson(
+    lesson_schema: CreateLessonRequestSchema,
+        course: Annotated[
+            Course,
+            Security(
+                CoursePermissionDependency(
+                    [
+                        IsAuthorCourse
+                    ],
+                )
+            )
+        ],
+    service: Annotated[LessonService, Depends(get_service(LessonService))],
+) -> LessonResponseSchema:
+    lesson = await service.create_lesson(
+        course=course, lesson_schema=lesson_schema
+    )
+    return LessonResponseSchema.model_validate(lesson)
