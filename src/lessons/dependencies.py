3c44194d43c00bf6a722e7efed7fb1826d7bc299
PitRella <serezha.sliva@gmail.com>
2025-08-24 22:51:56 +0300

    FEAT(lessons/dependencies) - Added get_course_by_id dependency function for lesson retrieval

---
+++ src/lessons/dependencies.py
@@ -0,0 +1,15 @@
+import uuid
+from typing import Annotated
+
+from fastapi import Depends
+
+from src.base.dependencies import get_service
+from src.lessons.models import Lesson
+from src.lessons.service import LessonService
+
+
+async def _get_course_by_id(
+        lesson_id: uuid.UUID,
+        service: Annotated[LessonService, Depends(get_service(LessonService))],
+) -> Lesson:
+    return await service.get_lesson(lesson_id)
