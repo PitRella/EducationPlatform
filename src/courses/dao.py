0349ba14e28e48c80945b0f0776599cd2773c597
PitRella <serezha.sliva@gmail.com>
2025-08-24 18:29:56 +0300

    FEAT(courses/dao) - Added get_course_with_lessons method with eager loading of lessons

--- src/courses/dao.py
+++ src/courses/dao.py
@@ -1,7 +1,23 @@
-from src.base.dao import BaseDAO
+from sqlalchemy.orm import selectinload
+
+from src.base.dao import BaseDAO, Model
 from src.courses.models import Course
 from src.courses.schemas import BaseCreateCourseRequestSchema
+from typing import Any
+
+from sqlalchemy import (
+    Result,
+    Select,
+    select,
+)
+
 
 
 class CourseDAO(BaseDAO[Course, BaseCreateCourseRequestSchema]):
-    pass
+    async def get_course_with_lessons(self, *filters: Any, **filters_by: Any) -> Course | None:
+        query: Select[Any] = (
+            select(self.model).where(*filters).filter_by(**filters_by).options(
+                selectinload(self.model.lessons))
+        )
+        result: Result[Any] = await self.session.execute(query)
+        return result.scalar_one_or_none()
