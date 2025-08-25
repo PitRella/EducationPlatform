cab9ea3c2ed8192296bc0afbed7e7a92969b97de
PitRella <serezha.sliva@gmail.com>
2025-08-25 23:52:22 +0300

    FEAT(users/dao) - Added get_author method with user relationship preloading to AuthorDAO class

--- src/users/dao/author.py
+++ src/users/dao/author.py
@@ -1,7 +1,26 @@
+from typing import Any
+
+from sqlalchemy.orm import selectinload
+
 from src.base.dao import BaseDAO
 from src.users import Author
 from src.users.schemas import CreateAuthorRequestSchema
-
+from sqlalchemy import (
+    Result,
+    Select,
+    select,
+)
 
 class AuthorDAO(BaseDAO[Author, CreateAuthorRequestSchema]):
-    pass
+    async def get_author(
+            self,
+            *filters: Any,
+            **filters_by: Any
+    ) -> Author | None:
+        query: Select[Any] = (
+            select(self.model).where(*filters).filter_by(**filters_by).options(
+                selectinload(self.model.user))
+        )
+        result: Result[Any] = await self.session.execute(query)
+        return result.scalar_one_or_none()
+
