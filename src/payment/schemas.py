3f17644d9b8e8f8c77961bc02a2ae3650f08b256
PitRella <serezha.sliva@gmail.com>
2025-08-29 23:41:53 +0300

    FEAT(payment/schemas) - Extended `CreatePaymentRequestSchema` with `PaymentMethodEnum` and `PaymentProviderEnum` fields

--- src/payment/schemas.py
+++ src/payment/schemas.py
@@ -2,10 +2,13 @@ import uuid
 
 from pydantic import BaseModel
 from src.base.schemas import BaseSchema
+from src.payment.enums import PaymentMethodEnum, PaymentProviderEnum
 
 
 class CreatePaymentRequestSchema(BaseModel):
     course_id: uuid.UUID
+    payment_method: PaymentMethodEnum
+    provider: PaymentProviderEnum
 
 
 class PaymentResponseSchema(BaseSchema):
