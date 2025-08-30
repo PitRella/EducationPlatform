c948bc4bdc875ada8d4c71006abd3462dba98f55
PitRella <serezha.sliva@gmail.com>
2025-08-30 22:41:05 +0300

    FEAT(payment/providers) - Extended `AbstractProvider` with `get_payment_status` and `cancel_payment` methods; added optional `metadata` parameter to `create_payment` method

--- src/payment/services/providers/base.py
+++ src/payment/services/providers/base.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
 from decimal import Decimal
+from typing import Optional, Any
 
 from src.courses.enums import CurrencyEnum
-from src.payment.enums import PaymentMethodEnum
+from src.payment.enums import PaymentMethodEnum, PaymentStatusEnum
 
 
 class AbstractProvider(ABC):
@@ -11,6 +12,15 @@ class AbstractProvider(ABC):
             self,
             amount: Decimal,
             currency: CurrencyEnum,
-            method: PaymentMethodEnum
+            method: PaymentMethodEnum,
+            metadata: Optional[dict[str, Any]] = None
     ) -> None:
+        pass
+
+    @abstractmethod
+    def get_payment_status(self, payment_id: str) -> PaymentStatusEnum:
+        pass
+
+    @abstractmethod
+    def cancel_payment(self, payment_id: str) -> None:
         pass
