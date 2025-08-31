72fe819c47f655d7e8851f2188b42afa277ee950
PitRella <serezha.sliva@gmail.com>
2025-08-31 11:18:15 +0300

    FEAT(payment/providers) - Enhanced `StripePaymentProviderService` with `payment_status`, `payment_cancel`, and `get_payment_status` methods; updated `create_payment` to return `PaymentResult` with optional `metadata`

--- src/payment/services/providers/stripe.py
+++ src/payment/services/providers/stripe.py
@@ -1,11 +1,13 @@
 from decimal import Decimal
+from typing import Any
 
 import stripe
 
 from src.courses.enums import CurrencyEnum
-from src.payment.enums import PaymentMethodEnum
+from src.payment.enums import PaymentMethodEnum, PaymentStatusEnum
 from src.payment.services.providers.base import AbstractProvider
 from src.settings import Settings
+from src.payment.dto import PaymentResult
 
 settings = Settings.load()
 
@@ -18,10 +20,21 @@ class StripePaymentProviderService(AbstractProvider):
             self,
             amount: Decimal,
             currency: CurrencyEnum,
-            method: PaymentMethodEnum
-    ) -> None:
-        stripe.PaymentIntent.create(
+            method: PaymentMethodEnum,
+            metadata: dict[str, Any] | None = None
+    ) -> PaymentResult:
+        stripe_result = stripe.PaymentIntent.create(
             amount=int(amount),
             currency=currency,
             payment_method_types=[method],
         )
+        return PaymentResult.from_object(stripe_result)
+
+    def payment_status(self, payment_id: str) -> str:
+        pass
+
+    def payment_cancel(self, payment_id: str) -> None:
+        pass
+
+    def get_payment_status(self, payment_id: str) -> PaymentStatusEnum:
+        pass
