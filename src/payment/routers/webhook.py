949a1085ced10a87df8ffeb7fb9c187f2431d1e1
PitRella <serezha.sliva@gmail.com>
2025-09-02 00:22:30 +0300

    FEAT(payment/router) - Added `webhooks_payment_router` with `/payment/webhooks/stripe` endpoint and integrated it into `main_api_router`

--- src/payment/routers/webhook.py
+++ src/payment/routers/webhook.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 webhooks_payment_router = APIRouter()
 
 
-@webhooks_payment_router.post("/webhooks/stripe")
+@webhooks_payment_router.post("/stripe")
 async def stripe_webhook(request: Request) -> None:
     logger.info("Received a webhook from stripe")
     payload = await request.body()
