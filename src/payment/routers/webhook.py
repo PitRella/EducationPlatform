e175b069949b6cebba0d47894fdb3884c8f5b247
PitRella <serezha.sliva@gmail.com>
2025-09-02 00:18:12 +0300

    FEAT(payment/router) - Added `webhooks_payment_router` with Stripe webhook endpoint and updated `__all__` for export clarity

---
+++ src/payment/routers/webhook.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+from fastapi.requests import Request
+import logging
+
+logger = logging.getLogger(__name__)
+
+webhooks_payment_router = APIRouter()
+
+
+@webhooks_payment_router.post("/webhooks/stripe")
+async def stripe_webhook(request: Request) -> None:
+    logger.info("Received a webhook from stripe")
+    payload = await request.body()
+    logger.info(f"Payload: %s", payload)
