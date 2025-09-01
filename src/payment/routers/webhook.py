e4fc4b92150a801f7bff5965a740dbf89b5cec22
PitRella <serezha.sliva@gmail.com>
2025-09-02 01:30:22 +0300

    FEAT(payment/webhooks) - Added Stripe webhook signature validation using `stripe.Webhook.construct_event` and integrated `WEBHOOK_SECRET_KEY` from settings

--- src/payment/routers/webhook.py
+++ src/payment/routers/webhook.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
 from fastapi.requests import Request
+import stripe
 import logging
 
+from src.settings import Settings
+
+settings = Settings.load()
 from src.payment.services.webhooks import StripeWebhookService
 
 logger = logging.getLogger(__name__)
@@ -12,5 +16,9 @@ webhooks_payment_router = APIRouter()
 @webhooks_payment_router.post("/stripe")
 async def stripe_webhook(request: Request) -> None:
     body: bytes = await request.body()
+    stripe_signature = request.headers['stripe-signature']
+    stripe.Webhook.construct_event(
+        body,stripe_signature,settings.stripe_settings.WEBHOOK_SECRET_KEY
+    )
     await StripeWebhookService.handle_webhook(body)
 
