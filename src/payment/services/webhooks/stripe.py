89354aafc1714143ba51e46d8c6d0c0de1249fbf
PitRella <serezha.sliva@gmail.com>
2025-09-18 11:43:17 +0300

    FEAT(payment/services/webhooks) - added dao to init params, for closing payments

--- src/payment/services/webhooks/stripe.py
+++ src/payment/services/webhooks/stripe.py
@@ -1,12 +1,29 @@
-import stripe
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.base.dao import BaseDAO
+from src.base.service import BaseService
 from src.payment.dto import StripeWebhookPayload
+from src.payment.models import Payment
+from src.payment.schemas import CreatePaymentRequestSchema
+from src.payment.services.payment import PaymentDAO
 
 
-class StripeWebhookService:
-    @classmethod
+class StripeWebhookService(BaseService):
+    def __init__(
+            self,
+            db_session: AsyncSession,
+            payment_dao: PaymentDAO | None = None,
+    ) -> None:
+        super().__init__(db_session)
+        self._payment_dao: PaymentDAO = payment_dao or BaseDAO[
+            Payment,
+            CreatePaymentRequestSchema
+        ](session=db_session, model=Payment)
+
     async def handle_webhook(
-            cls,
+            self,
             request_body: bytes
     ):
-        payload: StripeWebhookPayload = StripeWebhookPayload.from_body(request_body)
-
+        payload: StripeWebhookPayload = StripeWebhookPayload.from_body(
+            request_body
+        )
